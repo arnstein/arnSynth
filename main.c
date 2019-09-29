715592a73845e548d712dc65892a12fc1ff04079
arnstein <arnsteinkleven@gmail.com>
2019-09-29 22:03:05 +0200

    Add some nicer initializations and stuff

--- main.c
+++ main.c
@@ -1,38 +1,46 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <curses.h>
 #include <math.h>
 #include <stdbool.h>
 
-SDL_Texture* text = NULL;
+// Video
 SDL_Window *window = NULL;
-SDL_Rect message_rect;
 SDL_Renderer *renderer = NULL;
-double sine_value = 0;
-double sine_step_size;
+TTF_Font* font = NULL;
+SDL_Surface* surface = NULL;
+SDL_Texture* texture = NULL;
+SDL_Rect texture_rect;
+
+// Audio
 SDL_AudioDeviceID audio_device;
 SDL_AudioSpec audio_spec;
-SDL_Event sdl_event;
+double sine_value = 0;
+double sine_step_size;
 int sample_rate = 44100;
-bool running = true;
-double frequency_output = 440;
 double phase_double = 0;
 double octave = 1.0;
 float amplitude = 127.0;
+double note = 0.0;
+bool sustain = true;
 
-double note = 440.0;
-int sample_nr = 0;
+// Events
+SDL_Event sdl_event;
+bool running = true;
+
 
 void audio_callback(void *data, Uint8 *stream, int length) {
     for (int i = 0; i < length; i++) {
-        stream[i] = (Uint8) (amplitude * sin(sine_value))+127;
+        stream[i] = (Uint8) (amplitude * sin(sine_value));
         sine_value += sine_step_size;
     }
 }
 
 int init_sdl_audio() {
 
-    SDL_Init(SDL_INIT_AUDIO);
+    if (SDL_Init(SDL_INIT_AUDIO) != 0) {
+        return 1;
+    }
+
     SDL_AudioSpec want;
     SDL_zero(want);
     SDL_zero(audio_spec);
@@ -46,9 +54,9 @@ int init_sdl_audio() {
     audio_device = SDL_OpenAudioDevice(NULL, 0, &want, &audio_spec, 0);
 
     if (audio_device == 0) {
-        printf("Failed to open audio!\n");
         return 1;
     }
+
     SDL_PauseAudioDevice(audio_device, 0);
 
     return 0;
@@ -56,35 +64,50 @@ int init_sdl_audio() {
 
 int init_sdl_video() {
 
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) || TTF_Init() != 0) {
+        return 1;
+    }
 
-    window = SDL_CreateWindow("arnSynth", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	TTF_Font* font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24);
-	SDL_Color color = {120, 200, 100, 0};
+    window = SDL_CreateWindow("arnSynth", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    renderer = SDL_CreateRenderer(window, -1, 0);
 
-	SDL_Surface* text_surface = TTF_RenderText_Solid(font, "Keyboard layout:\n\
+	font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20);
+
+	SDL_Color color = {255, 255, 255};
+
+	surface = TTF_RenderText_Blended_Wrapped(font, "Keyboard layout:\n\
 		|   |   | |   |   |   |   | |   | |   |   |     | \n\
 		|   | W | | E |   |   | T | | Y | | U |   |     | \n\
 		|   |___| |___|   |   |___| |___| |___|   |     | \n\
 		|     |     |     |     |     |     |     |     | \n\
 		|  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  | \n\
 		|_____|_____|_____|_____|_____|_____|_____|_____| \n\n\
-        Octave up: Z\n\
-        Octave down: X\n", color);
+        Octave up: X\n\
+        Octave down: Z\n\
+        Toggle sustain: C\n", color, 10000);
 
-	SDL_Texture* text = SDL_CreateTextureFromSurface(renderer, text_surface);
 
-	message_rect.x = 0;
-	message_rect.y = 0;
-	message_rect.w = 640;
-	message_rect.h = 480;
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
 
+    int texture_width = 0;
+    int texture_height = 0;
+    SDL_QueryTexture(texture, NULL, NULL, &texture_width, &texture_height);
+    texture_rect = (SDL_Rect){0, 0, texture_width, texture_height};
+
+    return 0;
+}
+
+int quit() {
+    SDL_CloseAudioDevice(audio_device);
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
+    TTF_CloseFont(font);
+    TTF_Quit();
+    SDL_Quit();
 }
 
 void handle_keydown(SDL_Keysym* key) {
     switch(key->sym) {
-        printf("Switching note\n");
         case SDLK_ESCAPE:
             running = false;
             break;
@@ -128,11 +151,13 @@ void handle_keydown(SDL_Keysym* key) {
             note = 523.25;
             break;
         case SDLK_z:
-            octave *= 2.0;
-            break;
-        case SDLK_x:
             octave /= 2.0;
             break;
+        case SDLK_x:
+            octave *= 2.0;
+            break;
+        case SDLK_c:
+            sustain = !sustain;
     }
     sine_step_size = 2 * 3.14159 * note * octave / sample_rate;
 }
@@ -150,12 +175,20 @@ int main() {
             switch(sdl_event.type) {
                 case SDL_KEYDOWN:
                     handle_keydown(&sdl_event.key.keysym);
+                    break;
+                case SDL_KEYUP:
+                    if (!sustain) {
+                        note = 0;
+                    }
+                    sine_step_size = 2 * 3.14159 * note * octave / sample_rate;
+                    break;
             }
         }
 
-        SDL_RenderCopy(renderer, text, NULL, &message_rect);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, &texture_rect);
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_CloseAudioDevice(audio_device);
-    SDL_Quit();
+    quit();
 }
